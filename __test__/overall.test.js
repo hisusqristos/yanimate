ff861b9864e64a89f9b3fb58c07fa62d67792ec9
varya <varyadaser@gmail.com>
2023-01-31 19:36:55 +0400

    no rescript ;(

---
+++ __test__/overall.test.js
@@ -0,0 +1,6 @@
+describe("tool to animate. we might have frames, animation duration and frame frequency",()=>{
+    //Frames as an array,
+    // duration as a number (10 for 10 seconds)
+    // frame frequency as a number (12 for 12 frames per second)
+    describe("Animate(ArrarOfFrames, 10, 24)") // for example
+})
